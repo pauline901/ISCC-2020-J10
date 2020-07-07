87717e6c0914478a3a43ea09d8e376f033840ea9
Baur Pauline <paulinebaur02@gmail.com>
2020-07-07 14:31:31 +0200

    push

---
+++ EX_02/afficher-produit.php
@@ -0,0 +1,33 @@
+<?php
+
+function connect_to_database() {
+    $severname="localhost";
+    $username="root";
+    $password="";
+    $databasename="BaseTest01";
+
+
+try{
+    $pdo= new PDO("mysql:host=$severname;dbname=$databasename",$username,$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "<b>Vous êtes connecté</b>";
+    return  $pdo;
+}
+catch(PDOException $e) {
+    echo "<b>Vous n'êtes pas connecté</b>".$e->getMessage();
+}
+}
+
+$pdo = connect_to_database();
+
+$query=$pdo->query("SELECT * FROM produit")->fetchAll();
+
+echo '<ul>';
+foreach($query as $product)
+{
+    echo "<li>".$product['nom']."</li><br/>";
+}
+echo '</ul>';
+
+?>
